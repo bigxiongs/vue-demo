df382c1ca37668d29e3296f561bb352cbf7247b7
Rene <Deshan-Xiong@outlook.com>
2024-04-02 12:53:35 +0800

    goods item

--- src/apis/home.js
+++ src/apis/home.js
@@ -3,3 +3,5 @@ import axios from '@/utils/http.js'
 export const getBannerAPI = () => axios.get('/home/banner')
 export const findNewAPI = () => axios.get('/home/new')
 export const getHotAPI = () => axios.get('/home/hot')
+export const getGoodsAPI = () => axios.get('/home/goods')
+

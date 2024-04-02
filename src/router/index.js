37835d9c060a236f221f12ae7d98d7f1ce6e5be0
Rene <Deshan-Xiong@outlook.com>
2024-04-02 12:53:48 +0800

    imageview

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Layout from '@/views/layout/index.vue'
 import Home from '@/views/home/index.vue'
 import Category from '@/views/category/index.vue'
 import SubCategory from '@/views/sub-category/index.vue'
+import Detail from '@/views/detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,13 +25,18 @@ const router = createRouter({
           path: 'category/sub/:id',
           component: SubCategory,
         },
+        {
+          path: 'detail/:id',
+          component: Detail,
+        }
       ],
     },
     {
       path: '/login',
       component: Login
     },
-  ]
+  ],
+  scrollBehavior() { return { top: 0 } }
 })
 
 export default router

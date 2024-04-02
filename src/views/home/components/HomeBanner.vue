dbfc295cdc8cedf5e83c50190a3c3c148844966d
Rene <Deshan-Xiong@outlook.com>
2024-04-02 12:53:25 +0800

    homepanel

--- src/views/home/components/HomeBanner.vue
+++ src/views/home/components/HomeBanner.vue
@@ -10,7 +10,9 @@ onMounted(getBanner)
 <template>
   <div class="home-banner">
     <el-carousel height="500px">
-      <el-carousel-item v-for="item in bannerList" :key="item.id"><img :src="item.imgUrl" alt=""></el-carousel-item>
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
     </el-carousel>
   </div>
 </template>
